66a58c1797fdb5721e4fd76a407b83e4b8b17f70
kennethsam2005 <kennethsam553@gmail.com>
2026-06-11 17:45:14 +0100

    init commit

---
+++ src/Assignment2.java
@@ -0,0 +1,18 @@
+public class Assignment2 {
+    public static void main(String[] args){
+        System.out.println("              T                ");
+        System.out.println("             TTT              ");
+        System.out.println("            TTTTT             ");
+        System.out.println("           TTTTTTT            ");
+        System.out.println("          TTTTTTTTTT          ");
+        System.out.println("        TTTTTTTTTTTTT        ");
+        System.out.println("      TTTTTTTTTTTTTTTTTT     ");
+    }
+
+
+
+
+
+
+
+}
